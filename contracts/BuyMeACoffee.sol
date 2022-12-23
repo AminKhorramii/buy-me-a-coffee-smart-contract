777f568116cce03bb94d94ddc0d25f14b42eaf75
Amin Khorrami <103181743+AminKhorramii@users.noreply.github.com>
2022-12-23 01:22:31 +0100

    feat/ contract public functions

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -5,5 +5,59 @@ pragma solidity ^0.8.0;
 // import "hardhat/console.sol";
 
 contract BuyMeACoffee {
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
 
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    Memo[] memos;
+
+    address payable owner;
+
+    // Only called once on contract deployment
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a nice message from the coffee buyer
+     */
+    function buyCoffee(string memory _name, string memory _message)
+        public
+        payable
+    {
+        require(
+            msg.value > 0,
+            "you need to have some ETH to buy a coffee friend! :)"
+        );
+
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+
+    /**
+     * @dev send the entire balance stored in this contract to this owner
+     */
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+     * @dev retrieve all the memos
+     */
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
 }
